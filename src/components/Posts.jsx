c4754af227658617f11595fd3089c10d4eb5aa4b
allmylinux <allmylinux@gmail.com>
2019-05-27 18:50:06 +0300

    second day final - all works

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,12 +1,73 @@
 import React, {Component} from 'react';
-import Post from './Post';
+import User from './User';
+import InstaService from '../services/instaservice';
+// import InstaService from './instaservice';
+import ErrorMessage from './ErrorMessage';
 
 export default class Posts extends Component {
+  InstaService = new InstaService();
+  state = {
+    posts: [],
+    error: false
+  }
+  
+  componentDidMount() {
+    this.updatePosts();
+  }
+
+  updatePosts() {
+    this.InstaService.getAllPosts()
+    .then(this.onPostsLoaded)
+    .catch(this.onError);
+  }
+
+  onPostsLoaded = (posts) => {
+    this.setState({
+      posts,
+      error: false
+    })
+  }
+
+  onError = (err) => {
+    this.setState({
+      error: true
+    })
+  }
+
+  renderItems(arr) {
+    return arr.map((item) => {
+      const {name, altname, photo, src, alt, descr, id} = item;
+
+      return (
+        <div key={id} className="post">
+          <User
+            src={photo}
+            alt={altname}
+            name={name}
+            min />
+          <img src={src} alt={alt}></img>
+          <div className="post__name">
+            {name}
+          </div>
+          <div className="post__descr">
+            {descr}
+          </div>
+      </div>
+      )
+    })
+  }
+
   render() {
+    const {error, posts} = this.state;
+    if (error) {
+      return <ErrorMessage />
+    }
+
+    const items = this.renderItems(posts);
+
     return (
       <div className="left">
-        <Post alt="nature" src="http://www.youandthemat.com/wp-content/uploads/nature-2-26-17.jpg" />
-        <Post alt="nature" src="https://www.9skips.com/wp-content/uploads/2017/10/forests.jpg" />
+        {items}
       </div>
     )
   }
